8cce778041535673a3509b6bca9f3c43932813ba
kujin <1604323361@qq.com>
2020-05-27 17:37:43 +0800

    Initial commit

---
+++ MyBatis_01/src/main/java/my/moudel/TestMybatis.java
@@ -0,0 +1,25 @@
+package my.moudel;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class TestMybatis {
+    public static void main(String[] args) throws IOException {
+        //mybatis配置文件
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session=sqlSessionFactory.openSession();
+        //对应一个mapper中sql语句的id
+        List<Student> cs=session.selectList("selectStudent");
+        for (Student c : cs) {
+            System.out.println(c.getName());
+        }
+    }
+}
